ae744c74f55fae5b7ee54858ad5c25db363b9836
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-07-19 13:28:59 +0800

    file updated

--- Develop/utils/generate-markdown.js
+++ Develop/utils/generate-markdown.js
@@ -1,10 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function generateLicenseBadge(license) {
+function generateLicenseBadge(license){
 
   //converts - to % to allow viewing in url
   const licenseName = encodeURIComponent(license.replace('-', ' '))
-  return `!${license}(https://img.shields.io/badge/License-${licenseName}-green)`
+  return `![${license}-licensed](https://img.shields.io/badge/license-${licenseName}-green)`
 }
 
 
