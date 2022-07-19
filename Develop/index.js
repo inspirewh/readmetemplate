34d644e993dcf14ea57e2ab3e4bb357bed042699
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-07-20 04:57:57 +0800

    titles updated

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,10 @@
+// TGOALS: Generate a readme file for future projects
 //import inquirer
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generate-markdown');
+const fs = require('fs/promises');
+const parth = require('path');
 
-// TGOALS: Generate a readme file for future projects
 
 //Questions to ask:
 //What is the project title:
@@ -21,29 +23,70 @@ inquirer.prompt([
     {
         type: 'list',
         message: "license",
-        name: 'licence',
+        name: 'license',
         choices: [
             'MIT', 'GPL-v2', 'Apache-v2', 'No license', 
         ]
     },
     {
         type: 'input',
-        message: "github username",
-        name: 'github username',
+        message: "Description",
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: "Installation",
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: "Usage",
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: "Contribution",
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: "Test Instructions",
+        name: 'test',
+    },
+    
+    {
+        type: 'input',
+        message: "github",
+        name: 'github',
     }
+    {
+        type: 'input',
+        message: "email",
+        name: 'email',
+    }
+
 ]).then((ans) => {
     console.log(ans);
     const markdown = generateMarkdown(ans);
+    
+    //once asked
+    //generate readme file based on the responses
+    //in the output folder
 
-    console.log(markdown);
+    const outputPath = path.join(__dirname,'output', 'generated.md');
+
+    fs.writeFile(outputPath, markdown, 'utf-8')
+        .then(() => {
+            console.log('generated!')
+        });
+
 })
 
 
 
 
 
-//once asked
-//generate readme file based on the responses
-//in the output folder
+
+
 
 
